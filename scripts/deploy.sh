13c46136082cd6aa4576b425560adc89e144f2ff
chrisforrette <chris@chrisforrette.com>
2018-02-03 00:48:53 -0800

    Trying different Git target for deploy script for CircleCI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git push --force https://git.heroku.com/$ENV-food-trucks-app.git HEAD:refs/heads/master
+git push --force git@heroku.com:$ENV-food-trucks-app.git HEAD:refs/heads/master
